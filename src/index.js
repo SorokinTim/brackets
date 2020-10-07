a010a47136bab82a994c95871806db61ccce3b52
Tim Sorokin <timsorokin352@gmail.com>
2020-10-07 18:21:07 +0300

    feat: pass 15 tests of brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsArray = str.split("");
+
+  let tempArray = [];
+  outer: for (let i = 0; i < bracketsArray.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+
+      if (bracketsArray[i] == bracketsConfig[j][0]) {
+        tempArray.unshift(bracketsArray[i]);
+        continue outer;
+      }
+
+    }
+    let mirrorArray = createMirrorArray(tempArray, bracketsConfig);
+
+    if (mirrorArray.join("") === bracketsArray.slice(tempArray.length, mirrorArray.length * 2).join("") && mirrorArray.join("") != "") {
+      bracketsArray.splice(0, mirrorArray.length * 2);
+      tempArray = [];
+      i = -1;
+      continue;
+    }
+  }
+
+  for(let x = 0; x < bracketsArray.length; x++) {
+    if(bracketsArray[x] == bracketsArray[x+1]) {
+      bracketsArray.splice(x, x+2);
+    }
+  }
+  return !String(bracketsArray);
+}
+
+function createMirrorArray(arr, bracketsConfig) {
+  return arr.map((item) => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (bracketsConfig[i][0] === item) {
+        return bracketsConfig[i][1];
+      }
+    }
+  });
 }
